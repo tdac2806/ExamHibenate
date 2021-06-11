076d9a1775fa1f50fc5dca0c7ca258c2d7c86e13
Tristan Da Costa <tristandacosta@gmail.com>
2021-06-11 16:47:19 +0200

    Ajout du service Hotel

---
+++ src/main/java/com/Service/HotelService.java
@@ -0,0 +1,46 @@
+package com.Service;
+
+import java.util.List;
+
+import com.Model.Hotel;
+
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
+public class HotelService {
+
+   public boolean create(Hotel Hotel, Session session) {
+      session.save(Hotel);
+      return true;
+   }
+
+   public boolean delete(int id, Session session) {
+      Hotel h = session.get(Hotel.class, id);
+      if (h != null)
+         session.delete(h);
+      return true;
+   }
+
+   public boolean update(int id, String nom, String adresse, String Telephone, Session session) {
+      Hotel h = session.get(Hotel.class, id);
+      if (h != null) {
+         h.setNom(nom);
+         h.setAdresse(adresse);
+         h.setTelephone(Telephone);
+         session.update(h);
+      }
+      return true;
+   }
+
+   public Hotel findById(int id, Session session) {
+      Hotel h = session.get(Hotel.class, id);
+      session.get(Hotel.class, id);
+      return h;
+   }
+
+   public List<Hotel> findAll(Session session) {
+      Query q = session.createQuery("From Hotel");
+      List<Hotel> listes = q.list();
+      return listes;
+   }
+}
